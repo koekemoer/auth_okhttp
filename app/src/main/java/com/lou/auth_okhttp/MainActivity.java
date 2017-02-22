087c6c33a31ff477090fb7822854afe621754f07
sharknado5 <loukoekemoer@gmail.com>
2017-02-22 15:57:46 +0200

    first commit

---
+++ app/src/main/java/com/lou/auth_okhttp/MainActivity.java
@@ -0,0 +1,79 @@
+package com.lou.auth_okhttp;
+
+import java.io.IOException;
+
+import android.net.Credentials;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.Route;
+import okhttp3.Authenticator;
+import okhttp3.Request;
+
+import static okhttp3.Credentials.basic;
+//import okhttp3.Credentials;
+
+public class MainActivity extends AppCompatActivity {
+
+    private OkHttpClient client;
+    private String response;
+
+    String url = "https://app.dev.it.si/alchemy/api/1.0/login";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        client = new OkHttpClient();
+
+        attemptLogin(url);
+
+    }
+
+    private void loadContent() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    response = ApiCall.GET(client, RequestBuilder.buildUrl());
+                    Log.d("Response", response);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    private void attemptLogin(String url) {
+        new AsyncTask<String, Void, Void>() {
+            protected Void doInBackground(String... params) {
+                try {
+                    response = ApiCall.POST(
+                            client,
+                            params[0],
+                            RequestBuilder.LoginBody("harvey", "p"/*, "token"*/));
+
+                    Log.d("Response", response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute(url);
+    }
+
+
+}
---
+++ app/src/main/java/com/lou/auth_okhttp/RequestBuilder.java
@@ -0,0 +1,33 @@
+package com.lou.auth_okhttp;
+
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.RequestBody;
+
+/**
+ * Created by lou on 2017/02/21.
+ */
+
+public class RequestBuilder {
+
+    public static RequestBody LoginBody(String username, String password/*, String token*/) {
+        return new FormBody.Builder()
+                .add("action", "login")
+                .add("format", "json")
+                .add("username", username)
+                .add("password", password)
+                //.add("logintoken", token)
+                .build();
+    }
+
+    public static HttpUrl buildUrl() {
+        return new HttpUrl.Builder()
+                .scheme("https")
+                .host("app.dev.it.si")
+                .addPathSegment("alchemy")
+                .addPathSegment("api")
+                .addPathSegment("1.0")
+                .addPathSegment("login")
+                .build();
+    }
+}
